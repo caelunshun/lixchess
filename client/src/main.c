aeb8752ad4d7ba073fbe98a20b9513b2f7a4e798
caelunshun <caelum12321@gmail.com>
2019-07-02 15:32:39 -0600

    Work on build configuration for client

--- client/src/main.c
+++ client/src/main.c
@@ -1,7 +1,13 @@
 #include <chess.h>
+#include <vector.h>
 #include <stdio.h>
 
 int main() {
+    Vector vec = vector_init(sizeof(int));
+    int *f = malloc(sizeof(int));
+    *f = 10;
+    vector_push(&vec, f);
 
+    printf("f = %d\n", *(int*) vector_get(&vec, 0));
 }
 
